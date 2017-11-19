8ef67275731c29676bdb8d6b398afacd9aa93655
varunbhandary <varun.bhandary@gmail.com>
2017-11-19 19:42:47 +0530

    Add existing file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix function generates matrix object that will cache 
+## inverse of a matrix 
 
-## Write a short comment describing this function
+## makeCacheMatrix 
+## takes a invertible matrix 'x' as an input
+## returns a list containing functions to set the matrix , get the  matrix,
+## set the inverse and get the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+       Minv <- NULL
+       
+       #used when to reset the invertible matrix 
+       set <- function(y){
+               x <<- y
+               inv <<- NULL
+       }
+       get <- function() x
+       setinv <- function(inverse) Minv <<- inverse
+       getinv <- function() Minv
+       list(set = set, get = get, setinv = setinv, getinv = getinv)
+        }
 
-}
 
-
-## Write a short comment describing this function
+## cacheSolve function takes the o/p of the  makeCacheMatrix and returns the
+## inverse of the matrix which was inputted to the makeCacheMatrix function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        # Return a matrix that is the inverse of 'x'
+        Minv <- x$getinv()
+        
+        # Check if the inverse has already been calculated & return from cache 
+        # if it is present
+        if(!is.null(Minv)){
+                message("geting data from the cache")
+                return(Minv)
+        }
+        
+        #The below willonly execute if thecache is not present
+        matrix.data <- x$get()
+        Minv <- solve(matrix.data, ...)
+        
+        x$setinv(Minv)
+        
+        return(Minv)
 }
